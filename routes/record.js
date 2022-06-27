2f754f6e198056a8deb66f4ef30840cab6faf07f
senopriambodo <senopriambodo88@gmail.com>
2022-06-27 01:50:59 +0000

    mengerjakan backend

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: 34.128.123.47,
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: moneytrackerappdatabase,
+    password: jakarta@2022
 })
 
 router.get("/dashboard", (req, res) => {
